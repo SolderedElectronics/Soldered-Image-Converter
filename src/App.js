85722558f3a767ce54b469969129cda9fd55db79
Robert Sorić <robert.soric@gmail.com>
2025-04-24 07:31:07 +0200

    Starting work on web version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import './App.css'; // We'll create this CSS file next
+import './App.css';
+
+import logoSVG from './img/logo.svg';
+import steps from './img/steps.png';
+import dragdrop from './img/dragdrop.png';
 
 function App() {
   // Assume images are in the public/img folder
-  const logoPath = '/img/logo.svg';
+  const logoPath = require("./img/logo.png"); // Path relative to the JS file
   const stepsPath = '/img/steps.png';
   const dropIconPath = '/img/dragdrop.png'; // Using dragdrop.png for the inner icon as requested
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logoPath} className="App-logo" alt="Soldered Image Converter Logo" />
+        <img src={logoSVG} className="App-logo" alt="Soldered Image Converter Logo" />
         <div className="Board-selector">
           <label htmlFor="boardSelect">Select board:</label>
           <select id="boardSelect" name="board">
@@ -25,7 +29,7 @@ function App() {
           Soldered Image Converter is a simple tool used to generate Arduino-compatible C++ code for displaying images on displays from Soldered Electronics.
         </p>
 
-        <img src={stepsPath} className="Steps-image" alt="Steps: 1. Upload, 2. Convert, 3. Upload" />
+        <img src={steps} className="Steps-image" alt="Steps: 1. Upload, 2. Convert, 3. Upload" />
 
         <div className="Dropzone">
           <h2>Drag and drop your files here</h2>
@@ -33,7 +37,7 @@ function App() {
             Supported file types: .jpg, .png, .bmp <br />
             10 files max.
           </p>
-          <img src={dropIconPath} className="Dropzone-icon" alt="Drag and drop placeholder" />
+          <img src={dragdrop} className="Dropzone-icon" alt="Drag and drop placeholder" />
           <p className="Or-divider">OR</p>
           <button className="Browse-button">BROWSE</button>
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       }
     ]
   },
